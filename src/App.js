fb870c05ff3decc105f21ca5428f912d2fe98355
joanita-51 <nakityojoanita51@gmail.com>
2023-07-28 13:22:41 +0300

    Implementing update and delete functionality for Todo items

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ const initialState = { name: '', description: '' }
 const App = ({ signOut, user }) => {
   const [formState, setFormState] = useState(initialState)
   const [todos, setTodos] = useState([])
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedTodo1, setUpdatedTodo1] = useState({id:'', name:'', description:''});
 
   useEffect(() => {
     fetchTodos()
@@ -41,6 +43,40 @@ const App = ({ signOut, user }) => {
       console.log('error creating todo:', err)
     }
   }
+  const handleOpenEditDialog = (id, name, description)=>{
+    setUpdatedTodo1({id, name, description});
+    setIsEditing(true);
+  }
+  const handleCloseEditDialog = () => {
+    setIsEditing(false);
+  };
+
+
+  async function editTodo(id, updatedTodo){
+    try {
+      const input ={
+        id,
+        ...updatedTodo
+      }
+      const updatedTodos= todos.map((todo)=>
+      todo.id === id? {...todo, ...updatedTodo}: todo
+    );
+    setTodos(updatedTodos)
+    await API.graphql(graphqlOperation(updateTodo,{input}))
+      
+    } catch (error) {
+      console.error('Error updating todo:', error)
+    }
+  }
+
+  const handleSaveEdit = async()=>{
+    try {
+      await editTodo(updatedTodo1.id,{name:updatedTodo1.name, description:updatedTodo1.description})
+      handleCloseEditDialog();
+    }catch(error){
+      console.error("Error updating todo:", error);
+    }
+  }
 
   async function removeTodo(id){
     try {
@@ -54,9 +90,9 @@ const App = ({ signOut, user }) => {
 
   return (
     <View style={styles.container}>
-      {/* <Heading level={1}>Hello {user.username}</Heading> */}
+      {/* <Heading level={1}>Hello </Heading> */}
       <div style={styles.title}>
-        <Heading level={1}>Hello Joanita 👋!</Heading>
+        <Heading level={1}>Hello buddy 👋!</Heading>
         <Button style={styles.signOut} onClick={signOut}>Sign out</Button>
       </div>
       <h2 style={styles.h2}>Weekly Todos</h2>
@@ -65,20 +101,42 @@ const App = ({ signOut, user }) => {
         <div>
           <TextField
             onChange={event => setInput('name', event.target.value)}
-            style={styles.input}
+            style={styles.TextField}
             value={formState.name}
             placeholder="Name"
           />
           <TextField
             onChange={event => setInput('description', event.target.value)}
-            style={styles.input}
+            style={styles.TextField}
             value={formState.description}
             placeholder="Description"
           />
           <button style={styles.button} onClick={addTodo}>Create Todo</button>
         </div>
 
-        <div>
+        <div >
+          {isEditing && (
+            <div style={styles.update}>
+              <h2 style={styles.titleEdit}>Edit Todo</h2>
+              <TextField
+                style={styles.TextField}
+                type="text"
+                value={updatedTodo1.name}
+                onChange={(e) => setUpdatedTodo1({ ...updatedTodo1, name: e.target.value })}
+                placeholder="Enter new name"
+              />
+              <TextField
+                style={styles.TextField}
+                type="text"
+                value={updatedTodo1.description}
+                onChange={(e) => setUpdatedTodo1({ ...updatedTodo1, description: e.target.value })}
+                placeholder="Enter new description"
+              />
+              <Button onClick={() => handleSaveEdit()} style={styles.edit}>Save</Button>
+              <Button onClick={handleCloseEditDialog} style={styles.delete}>Cancel</Button>
+            </div>
+          )}
+
           {
             todos.map((todo, index) => (
               <View key={todo.id ? todo.id : index} style={styles.todo}>
@@ -93,7 +151,7 @@ const App = ({ signOut, user }) => {
                 </div>
 
                 <div>
-                  <Button style={styles.edit}>Edit</Button>
+                  <Button style={styles.edit} onClick={() => handleOpenEditDialog(todo.id, todo.name, todo.description)}>Edit</Button>
                   <Button style={styles.delete} onClick={()=>removeTodo(todo.id)}>Delete</Button>
                 </div>
 
@@ -123,13 +181,15 @@ const styles = {
   todoItems:{display:'flex',justifyContent: 'space-between', marginBottom:15,},
   edit:{width:100, marginRight:15, backgroundColor:'#F79327', color:'white'},
   delete:{marginLeft:15, backgroundColor:'#F24C3D', color:'white', width:100},
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18},
+  TextField: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18},
   todoName: { fontSize: 20, fontWeight: 'bold', paddingLeft:10 },
   todoDescription: { marginBottom: 0, paddingLeft:10  },
   button: { backgroundColor: '#22A699', color: 'white', borderRadius:10, fontSize: 18, padding: '12px', borderColor:'none' },
   signOut:{backgroundColor:'#F24C3D', color:'white', borderColor:'#F29727'},
   todoNumber:{paddingTop:5},
-  text:{color:'white'}
+  text:{color:'white'},
+  titleEdit:{marginTop:0},
+  update:{marginBottom:20}
 }
 
 export default withAuthenticator(App)
